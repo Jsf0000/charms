d34d18c259ebe40de73091f45dd32ef9b17c6f47
Jason Solano <jmsolano@akurey.com>
2020-11-23 18:00:38 -0600

    Install msyql server

--- builds/mysql-example/reactive/layer_example.py
+++ builds/mysql-example/reactive/layer_example.py
@@ -9,32 +9,12 @@ from charmhelpers.core.templating import render
 def install_layer_example():
     set_flag('mysql-example.installed')
     
-@when('apt.installed.hello')
-def set_message_hello():
-    application_version_set(get_upstream_version('hello'))
+#Install mysql server
+@when('apt.installed.mysql-server')
+def set_message_mysql_server():
+    application_version_set(get_upstream_version('mysql-server'))
+   
+    status_set('maintenance', 'Mysql Installed' )
     
-    message = sp.check_output('hello', stderr=sp.STDOUT)
-
-    status_set('maintenance', message )
+    set_flag('mysql-server.version.set')
     
-    set_flag('hello.version.set')
-    
-@when('database.available')
-def write_text_file(mysql):
-    render(source='text-file.tmpl',
-           target='/root/text-file.txt',
-           owner='root',
-           perms=0o775,
-           context={
-               'my_database': mysql,
-           })
-    status_set('active', 'Ready: File rendered.')
-
-@when_not('database.connected')
-def missing_mysql():
-    status_set('blocked', 'Please add relation to MySQL')
-
-@when('database.connected')
-@when_not('database.available')
-def waiting_mysql(mysql):
-    status_set('waiting', 'Waiting for MySQL')
