4c25137aa24fbceac1dc8823b344fc85ce30153e
Jason Solano <jmsolano@akurey.com>
2020-11-23 18:53:39 -0600

    Last changes

--- layers/layer-example/reactive/layer_example.py
+++ layers/layer-example/reactive/layer_example.py
@@ -18,3 +18,24 @@ def set_message_mysql_server():
     
     set_flag('mysql-server.version.set')
     
+@when('database.available')
+def write_text_file(mysql):
+    render(source='text-file.tmpl',
+           target='/root/text-file.txt',
+           owner='root',
+           perms=0o775,
+           context={
+               'my_database': mysql,
+           })
+    status_set('active', 'Ready: File rendered.')
+
+
+@when_not('database.connected')
+def missing_mysql():
+    status_set('blocked', 'Please add relation to MySQL')
+
+
+@when('database.connected')
+@when_not('database.available')
+def waiting_mysql(mysql):
+    status_set('waiting', 'Waiting for MySQL')
